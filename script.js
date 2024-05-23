20b15acaf1234dcfffc0b17c08a61362455883ca
Rylan Sedlacek <99748019+rylansedlacek@users.noreply.github.com>
2024-05-23 08:34:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, setDoc, getDocs } from "firebase/firestore";
+//import { initializeApp } from "firebase/app";
+//import { getFirestore, collection, doc, setDoc, getDocs } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
